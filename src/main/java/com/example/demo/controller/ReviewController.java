48dd05087fc9c475a6be2d25dbd5e2a487db3280
ADMIN <mehernoshp@yahoo.com>
2019-11-29 12:47:59 +0530

    As per Request # 2 - added anotherMessage

--- src/main/java/com/example/demo/controller/ReviewController.java
+++ src/main/java/com/example/demo/controller/ReviewController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,6 +11,11 @@ public class ReviewController {
     public String getMessage()
     {
         return "Hello World";
-        
+    }
+
+    @GetMapping(value = "/message/{name}")
+    public String anotherMessage(@PathVariable)("name" String )
+    {
+        return "Hello " + name;
     }
 }
